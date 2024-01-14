c993784b7cb11c377437eb8a2b3e9b93e10f87b6
Peta <peta@MacBook-Pro-de-Peta.local>
2024-01-14 18:23:40 +0000

    defining some of the conditions

--- src/main/java/com/mycompany/studentsca1/StudentsCA1.java
+++ src/main/java/com/mycompany/studentsca1/StudentsCA1.java
@@ -28,15 +28,35 @@ public class StudentsCA1 {
             while (studentsFile != null) {
                 studentsFile = br.readLine();
             }
-            
+
             br.close();
             
 
             // Storing each line from the txt file in a variable
             List<String> lines = Files.lines(Paths.get("students.txt")).collect(Collectors.toList());
+            
+            // Full name
             String studentFullName = lines.get(0);
+            // Number of classes
+            // CREATE EXCEPTION FOR THIS INPUT TO BE A POSITIVE INTEGER NUMBER
             Integer numberOfClasses = Integer.parseInt(lines.get(1));
+            // ID
             String studentsID = lines.get(2);
+            
+            // Storing the first two chars of the ID to be able to check the conditions
+            String studentsIDFirstTwoChars = studentsID.substring(0,2);
+            // Storing the middle chars of the ID to be able to check the conditions
+            String studentsIDMiddleChars;
+            // Storing the last chars of the ID to be able to check the conditions
+            String studentsIDLastChars;
+            
+            
+            // Condition to make sure there is a dash in the file
+            // before declaring different variables based on this dash.
+            if (!studentFullName.contains(" ")) {
+                System.out.println("Please include a space ' ' between the name and the surname.\n"); 
+            }
+            
             
             // Finding where the position of the space is to see where the name ends
             int locationSpace = studentFullName.indexOf(" ");
@@ -47,17 +67,27 @@ public class StudentsCA1 {
             // Storing name and surname in separate variables
             String name = studentFullName.substring(0,locationSpace);
             String surname = studentFullName.substring(locationSpace+1,locationLastChar);
+
+            // Exception handling.
+            // 1st condition to make sure that the first name is in letters.
+            if (!name.matches("[a-zA-Z]+")) {
+                System.out.println("The first name must be letters only.");
+                // 2nd condition to make sure that the surname is letters and/or numbers.
+                } else if (!surname.matches("[a-zA-Z0-9]+")) {
+                    System.out.println("The surname must be letters and/or numbers.");
+                    // 3rd condition to make sure that the number of classes is between 1 and 8 (inclusive).
+                    // No need to do a regex for positive integers as this has already been parsed as integer.
+                    } else if (numberOfClasses < 0 || numberOfClasses > 8) {
+                        System.out.println("The number of classes must be an integer value between 1 and 8 (inclusive).");
+                        // 4th condition to make sure that the first two chars of the student ID number are positive integers.
+                        } else if (!studentsIDFirstTwoChars.matches("[0-9]+")) {
+                                System.out.println("The first two chars of the student ID number must be positive integer numbers.");
+                            }
             
 
-            System.out.println(studentFullName);
-            System.out.println(numberOfClasses);
-            System.out.println(studentsID);
-            System.out.println(name);
-            System.out.println(surname);
             
-
         } catch (Exception e) {
-            System.out.println("error");
+            System.out.println(e);
         }
     }
 }
