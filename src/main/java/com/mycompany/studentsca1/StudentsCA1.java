0fe30fa726b8c9cd8425d398875c2c08306a268e
Peta <peta@MacBook-Pro-de-Peta.local>
2024-01-14 17:15:18 +0000

    storing each line from the txt file in a variable

--- src/main/java/com/mycompany/studentsca1/StudentsCA1.java
+++ src/main/java/com/mycompany/studentsca1/StudentsCA1.java
@@ -7,6 +7,10 @@ package com.mycompany.studentsca1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -22,11 +26,20 @@ public class StudentsCA1 {
             String studentsFile = br.readLine();
             
             while (studentsFile != null) {
-                System.out.println(studentsFile);
                 studentsFile = br.readLine();
             }
             
             br.close();
+            
+            // Storing each line from the txt file in a variable
+            List<String> lines = Files.lines(Paths.get("students.txt")).collect(Collectors.toList());
+            String studentsName = lines.get(0);
+            Integer numberOfClasses = Integer.parseInt(lines.get(1));
+            String studentsID = lines.get(2);
+            
+            System.out.println(studentsName);
+            System.out.println(numberOfClasses);
+            System.out.println(studentsID);
             
 
         } catch (Exception e) {
