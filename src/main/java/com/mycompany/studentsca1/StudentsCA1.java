a931f9c8a1677ec217991e7611426237cfa17368
Peta <peta@MacBook-Pro-de-Peta.local>
2024-01-28 14:26:27 +0000

    program now reads all students but stops printing when one is wrong, reviewing

--- src/main/java/com/mycompany/studentsca1/StudentsCA1.java
+++ src/main/java/com/mycompany/studentsca1/StudentsCA1.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -21,30 +20,30 @@ import java.util.stream.Collectors;
  */
 public class StudentsCA1 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         
         try {
             // Reading the students.txt file
             BufferedReader br = new BufferedReader(new FileReader("students.txt"));
             String studentsFile = br.readLine();
             
-            while (studentsFile != null) {
-                studentsFile = br.readLine();
-            }
-            br.close();
-            
-
-            // Storing each line from the txt file into a variable
-            List<String> lines = Files.lines(Paths.get("students.txt")).collect(Collectors.toList());
-
             // Starting point of txt file lines
             int lineZero = 0;
             int lineOne = 1;
             int lineTwo = 2;
             
             
+            while (studentsFile != null) {
+                studentsFile = br.readLine();
+                
+
+            // Storing each line from the txt file into a variable
+            List<String> lines = Files.lines(Paths.get("students.txt")).collect(Collectors.toList());
+
+            
             /// FULL NAME ///
             String studentFullName = lines.get(lineZero);
+            
             
             // Condition to make sure there is a space in the file
             // before declaring different variables based on this space.
@@ -66,12 +65,13 @@ public class StudentsCA1 {
             /// NUMBER OF CLASSES ///
             // CREATE EXCEPTION FOR THIS INPUT TO BE A POSITIVE INTEGER NUMBER
             String numberOfClasses = (lines.get(lineOne));
+            
             int numberOfClassesInt = Integer.parseInt(numberOfClasses);
             
 
-            
             /// STUDENT NUMBER ///
             String studentNumber = lines.get(lineTwo);
+            
             
             // Using split method and regex to store the different characters into variables
             // to be able to check the appropriate conditions
@@ -117,65 +117,47 @@ public class StudentsCA1 {
                                 // No need to do a regex for positive integers as this has already been parsed as integer.
                                 } else if (studentNumberLastCharsInt < 1 || studentNumberLastCharsInt > 200) {
                                     System.out.println("Last number of the student number has to be an integer number between 1 and 200.");
-                                    // If all conditions are met, I create the output txt file
-                                    } else {
-                                        String studentNumberFinal = studentNumberFirstTwoChars + studentNumberMiddleChars + studentNumberLastChars;
-                                        
-                                        BufferedWriter bw = new BufferedWriter(new FileWriter("status.txt", true));
-                                        bw.write(studentNumberFinal + " - ");
-                                        bw.close();
-                                        System.out.println("The student number has been added to the status.txt file");
-                                        lineTwo += 3;
+                                    // Condition to make sure that the first name is in letters.
+                                    } else if (!name.matches("[a-zA-Z]+")) {
+                                        System.out.println("The first name must be letters only.");
+                                        // Condition to make sure that the surname is letters and/or numbers.
+                                        } else if (!surname.matches("[a-zA-Z0-9]+")) {
+                                            System.out.println("The surname must be letters and/or numbers.");
+                                            // Condition to make sure that the number of classes is an integer NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                                            } else if (!numberOfClasses.matches("^[1-9]\\d*$")) {
+                                                System.out.println("The number of classes must be an integer value between 1 and 8 (inclusive).");
+                                                // Condition to make sure that the number of classes is between 1 and 8 (inclusive).
+                                                // No need to do a regex for positive integers as this has already been parsed as integer.
+                                                } else if (numberOfClassesInt < 0 || numberOfClassesInt > 8) {
+                                                    System.out.println("The number of classes must be an integer value between 1 and 8 (inclusive).");
+                                                    } else {
+
+                                                        // Declaring a variable for the workload based on the number of clases
+                                                        String workload = "";
+
+                                                        if (numberOfClassesInt == 1) {
+                                                            workload = "Very Light";
+                                                            } else if (numberOfClassesInt == 2) {
+                                                                workload = "Light";
+                                                                } else if (numberOfClassesInt >= 3 && numberOfClassesInt <= 5) {
+                                                                    workload = "Part Time";
+                                                                    } else if (numberOfClassesInt >= 6) {
+                                                                        workload = "Full Time";
+                                                                        }
+                                                        
+                                                        // If all conditions are met, I create the output txt file
+                                                        BufferedWriter bw = new BufferedWriter(new FileWriter("status.txt", true));
+                                                        bw.write(studentNumber + " - " + surname + "\n");
+                                                        bw.write(workload + "\n");
+                                                        bw.close();
+                                                        
+                                                        lineZero += 3;
+                                                        lineOne += 3;
+                                                        lineTwo += 3;
+                                                }
             }
-            
 
-            // Condition to make sure that the first name is in letters.
-            if (!name.matches("[a-zA-Z]+")) {
-                System.out.println("The first name must be letters only.");
-            // Condition to make sure that the surname is letters and/or numbers.
-                } else if (!surname.matches("[a-zA-Z0-9]+")) {
-                    System.out.println("The surname must be letters and/or numbers.");
-                    } else {
-                        String surnameFinal = surname;
-
-                        BufferedWriter bw = new BufferedWriter(new FileWriter("status.txt", true));
-                                                bw.write(surnameFinal + "\n");
-                                                bw.close();
-                                                System.out.println("The surname has been added to the status.txt file");
-                                                
-                        
-                    }
-            
-            
-            // Condition to make sure that the number of classes is an integer NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            if (!numberOfClasses.matches("^[1-9]\\d*$")) {
-                System.out.println("The number of classes must be an integer value between 1 and 8 (inclusive).");
-                // Condition to make sure that the number of classes is between 1 and 8 (inclusive).
-                // No need to do a regex for positive integers as this has already been parsed as integer.
-                } else if (numberOfClassesInt < 0 || numberOfClassesInt > 8) {
-                    System.out.println("The number of classes must be an integer value between 1 and 8 (inclusive).");
-                    } else {
-
-                        int numberOfClassesFinal = numberOfClassesInt;
-                        // Declaring a variable for the workload based on the number of clases
-                        String workload = "";
-
-                        if (numberOfClassesFinal == 1) {
-                            workload = "Very Light";
-                            } else if (numberOfClassesFinal == 2) {
-                                workload = "Light";
-                                } else if (numberOfClassesFinal >= 3 && numberOfClassesFinal <= 5) {
-                                    workload = "Part Time";
-                                    } else if (numberOfClassesFinal >= 6) {
-                                        workload = "Full Time";
-                                        }
-                        
-                        BufferedWriter bw = new BufferedWriter(new FileWriter("status.txt", true));
-                        bw.write(workload + "\n");
-                        bw.close();
-                        System.out.println("The workload has been added to the status.txt file");
-                        lineOne += 3;
-                }
+            br.close();
 
                         
         } catch (Exception e) {
